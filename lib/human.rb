f107e7e5d15be9e222a4fd7798f3c5198fd39a98
matugm <matugm@gmail.com>
2015-11-17 23:15:16 +0100

    Refactor move method

--- lib/human.rb
+++ lib/human.rb
@@ -8,28 +8,34 @@ module HumanMovement
     @human   = human
 
     set_random_direction
-
-    new_direction = @board.find_wall(@human)
-
-    go_down_and_update_direction(new_direction) if new_direction && human_can_move?
-
-    return if new_direction
-    return if overlapping?
-
-    @display.update { human.send("move_#{@random_direction}") }
+    move_human_if_possible
   end
 
   def set_random_direction
     @random_direction = %w(left right).sample
   end
 
+  def move_human_if_possible
+    if new_direction && human_can_move?
+      go_down_and_update_direction
+    end
+
+    unless new_direction || overlapping?
+      @display.update { @human.send("move_#{@random_direction}") }
+    end
+  end
+
+  def new_direction
+    @board.find_wall(@human)
+  end
+
   def human_can_move?
     !(@board.last_row?(@human) || @board.position_used?(@human.x+1, @human.y))
   end
 
-  def go_down_and_update_direction(direction)
+  def go_down_and_update_direction
     @display.update { @human.move_down }
-    @display.update { @human.send("move_#{direction}") }
+    @display.update { @human.send("move_#{new_direction}") }
   end
 
   def overlapping?
