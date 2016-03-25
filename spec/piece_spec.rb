051a194c7c260652a99544fabe76f42319b8776a
matugm <matugm@gmail.com>
2016-03-25 17:24:37 +0100

    Improve use of parenthesis

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,13 +1,13 @@
 require 'piece'
 
 describe Piece do
-  let(:piece) { Human.new() }
+  let(:piece) { Human.new }
 
   it 'can move one position to the left' do
     initial_position = piece.y
 
     piece.move_left
-    expect(piece.y).to eq initial_position - 1
+    expect(piece.y).to eq (initial_position - 1)
   end
 
   it 'returns an appropiate symbol' do
